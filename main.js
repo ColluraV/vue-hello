caa7e42e77387d32ed4491654f501c0d615edb16
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-19 17:49:14 +0200

    completo

--- main.js
+++ main.js
@@ -21,21 +21,26 @@ createApp ({
           
             if (this.contatore <= 3){
                 this.messaggio = "OTTIMO FUNZIONA!!";
-                mieImg = `img/meme_${this.contatore}.jpg`;
-                console.log(mieImg)
+                this.mieImg = `img/meme_${this.contatore}.jpg`;
+
+                
             }
+            
+            if (this.contatore == 1){
+                this.sub = "premi il pulsante per continuare";
+
+            }
+
             if (this.contatore == 2){
                 this.messaggio = "Non esagerare che poi si rompe";
                 this.sub = "se proprio ci tieni premi ancora"
             }
 
             if (this.contatore == 3){
-                this.messaggio = "Non esagerare che poi si rompe";
-                this.sub = "se proprio ci tieni premi ancora"
+                this.messaggio = "Vabbè spacchiamo tutto";
+                this.sub = "fai cosa vuoi"
                 this.contatore=0;
             }
-
-
         }
     /*<input @keyup.enter="onEnter" />*/
 
