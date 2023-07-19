69ebfa2222df352e99d5521d6d8e450eab8803da
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-19 17:08:50 +0200

    aggiunte funzioni non legge src:"" in html

--- main.js
+++ main.js
@@ -3,9 +3,43 @@ const {createApp} = Vue;
 createApp ({
 
     data(){
-        messaggio:"Benvenuto in Vue"
         
-    }
+        return{
+            contatore: 0,
+            messaggio:"Benvenuto in Vue",
+            sub : "premi il pulsante per continuare",
+            mieImg:"",
+        }
+    },
+    
+    methods:{
 
+        pressBtn: function(){
+
+            this.contatore ++;
+            console.log (this.contatore);
+          
+            if (this.contatore <= 3){
+                this.messaggio = "OTTIMO FUNZIONA!!";
+                mieImg = `img/meme_${this.contatore}.jpg`;
+                console.log(mieImg)
+            }
+            if (this.contatore == 2){
+                this.messaggio = "Non esagerare che poi si rompe";
+                this.sub = "se proprio ci tieni premi ancora"
+            }
+
+            if (this.contatore == 3){
+                this.messaggio = "Non esagerare che poi si rompe";
+                this.sub = "se proprio ci tieni premi ancora"
+                this.contatore=0;
+            }
+
+
+        }
+    /*<input @keyup.enter="onEnter" />*/
+
+    }
+    
 
 }).mount('#app')
