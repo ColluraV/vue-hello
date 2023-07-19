4a0cc2c4c71062e5a8412eba5d6e23a9edaab174
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-19 18:02:20 +0200

    commentato

--- main.js
+++ main.js
@@ -13,38 +13,40 @@ createApp ({
     },
     
     methods:{
-
+        //Fuonzione click bottone
         pressBtn: function(){
 
             this.contatore ++;
             console.log (this.contatore);
           
             if (this.contatore <= 3){
+
                 this.messaggio = "OTTIMO FUNZIONA!!";
                 this.mieImg = `img/meme_${this.contatore}.jpg`;
-
                 
             }
-            
+            //scenario 1
             if (this.contatore == 1){
+                
                 this.sub = "premi il pulsante per continuare";
 
             }
-
+            //scenario 2
             if (this.contatore == 2){
+                
                 this.messaggio = "Non esagerare che poi si rompe";
                 this.sub = "se proprio ci tieni premi ancora"
             }
-
+            //scenario 3
             if (this.contatore == 3){
+                
                 this.messaggio = "Vabbè spacchiamo tutto";
                 this.sub = "fai cosa vuoi"
                 this.contatore=0;
+            
             }
         }
-    /*<input @keyup.enter="onEnter" />*/
-
-    }
+    },
     
 
 }).mount('#app')
